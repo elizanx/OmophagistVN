510c11afad5713af5470309feca900483017a313
eliza <elizanabibaks@gmail.com>
2024-06-12 14:02:11 +0200

    name generator

---
+++ OmophagistVN/Assets/Scripts/Text/Display Text.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DisplayText : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshProUGUI;
+    public Text obj_text;
+    public InputField Display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj_text.text = PlayerPrefs.GetString("user_name");
+    }
+
+    public void Create()
+    {
+        obj_text.text = Display.text;
+        PlayerPrefs.SetString("user_name", obj_text.text);
+        PlayerPrefs.Save();
+    }
+    void Update()
+    {
+        
+    }
+}
