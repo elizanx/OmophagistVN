9b7f74a2e8aadc2d38470798c1e4db553251578f
JoyceMolenaar0 <146432917+JoyceMolenaar0@users.noreply.github.com>
2024-06-24 16:50:56 +0200

    BGChangerScene

--- OmophagistVN/Assets/Scripts/Controllers/BackgroundChanger.cs
+++ OmophagistVN/Assets/Scripts/Controllers/BackgroundChanger.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngineInternal;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class BackgroundChanger : MonoBehaviour
 {
 
     public List<Sprite> backgrounds;
     private Image image;
+    private string SceneName;
     // Start is called before the first frame update
     void Start()
     {
         Dialogue.OnSentenceIncrement += ChangeBackground;
         image = GetComponent<Image>();
-
+        SceneName = SceneManager.GetActiveScene().name;
 
 
     }
@@ -22,20 +25,33 @@ public class BackgroundChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void ChangeBackground(int index)
+
+
+
+    //Eliza, als je wilt dat het dus bij alle scene's anders is moet je gewoon dit erbij doen:
+    //Normaal: if (SceneName == "(jouw scene naam)" && index == (natuurlijk bij welke zin jij het wilt) )
+     // {
+     //     Debug.Log("hoi");
+     //     image.sprite = backgrounds[0];
+     // }
+
+void ChangeBackground(int index)
     {
         
 
-      if (index == 2)
+      if ( index == 2)
       {
             Debug.Log("hoi");
             image.sprite = backgrounds[0];
       }
-        if (index == 3) {
-            image.sprite = backgrounds[1];
-        }
+
+      if (index == 3)
+      {
+            Debug.Log("Dit is de 3e zin");
+            image.sprite = backgrounds[2];
+      }
     }
 }
