a5830ba9c5870405137ad465d8d64a88bc006090
eliza <elizanabibaks@gmail.com>
2024-06-29 15:36:07 +0200

    Revert "nothing"
    This reverts commit c4c3700416651ccdf9b348dc85021f9c8454e2d5.

--- OmophagistVN/Assets/Scripts/Controllers/LoadScript/LoadScript.cs
+++ OmophagistVN/Assets/Scripts/Controllers/LoadScript/LoadScript.cs
@@ -10,7 +10,7 @@ public class LoadScript : MonoBehaviour
         {
             
             string savedSceneName = PlayerPrefs.GetString("SavedScene");
-            SceneManager.LoadScene(savedSceneName);
+            SceneManager.LoadScene(savedSceneName);3
             Debug.Log("Game geladen! Opgeslagen scene: " + savedSceneName);
         }
         else
